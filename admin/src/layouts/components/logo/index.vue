af01ee9c293d6783b02e4efd028dc5b05656d682
Hancel.Lin <imlinhanchao@gmail.com>
2025-05-30 19:11:10 +0800

    ✨ feat: 更新logo图标

--- admin/src/layouts/components/logo/index.vue
+++ admin/src/layouts/components/logo/index.vue
@@ -10,7 +10,7 @@
 
 <template>
   <div class="flex justify-center h-full py-3 cursor-pointer" @click="goHome">
-    <Icon icon="i-ph:sword" :size="30" />
+    <Icon icon="svg-icon:logo" :size="30" />
     <div class="ml-2 truncate md:opacity-100 text-2xl font-bold">
       {{ title }}
     </div>
