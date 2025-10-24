75b29d6b14ffb234583d118cdd72d0d183eb9a2e
Hancel.Lin <imlinhanchao@gmail.com>
2025-10-24 13:30:23 +0800

    🐞 fix: 修正缩放后移动场景鼠标不跟手问题

--- admin/src/views/scene/scene.vue
+++ admin/src/views/scene/scene.vue
@@ -25,8 +25,8 @@
       x: (e as MouseEvent).clientX || (e as TouchEvent).touches[0].clientX,
       y: (e as MouseEvent).clientY || (e as TouchEvent).touches[0].clientY,
     }
-    beginPos.value.x = client.x * props.zoom - data.value.position.x;
-    beginPos.value.y = client.y * props.zoom - data.value.position.y;
+    beginPos.value.x = client.x - data.value.position.x * props.zoom;
+    beginPos.value.y = client.y - data.value.position.y * props.zoom;
   }
 
   useEventListener({
@@ -34,8 +34,8 @@
     name: 'mousemove',
     listener: (e: MouseEvent) => {
       if (isMove.value) {
-        data.value.position.x = e.clientX * props.zoom - beginPos.value.x;
-        data.value.position.y = e.clientY * props.zoom - beginPos.value.y;
+        data.value.position.x = (e.clientX - beginPos.value.x) / props.zoom;
+        data.value.position.y = (e.clientY - beginPos.value.y) / props.zoom;
       }
     },
     wait: 0,
